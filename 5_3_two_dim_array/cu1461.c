a0a90b6a1d21e9e00121ecc4caa32d4b8160ba94
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-19 22:57:03 +0900

    cu1461

---
+++ 5_3_two_dim_array/cu1461.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[100][100];
+    int n;
+    int cnt = 1;
+    scanf("%d",&n);
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = n-1 ; j >= 0 ; j--)
+        {
+            a[i][j] = cnt;
+            cnt++;
+        }
+    }
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
