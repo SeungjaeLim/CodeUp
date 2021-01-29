5f91aaf2918934976e07bfb9549c1402a54bc4af
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-29 23:24:08 +0900

    Solve cu1106

---
+++ 1_print/cu1106.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int range = -1;
+    for( int i = 1 ; i <= 31 ; i++ )
+    {
+        range = range * 2;
+    }
+    printf("%d %d", range, -(range+1));
+}
+
