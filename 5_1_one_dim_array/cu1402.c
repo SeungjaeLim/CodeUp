8e437739e0577c523dc2d543c3219c218c11d984
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-29 23:11:27 +0900

    Solve cu1402

---
+++ 5_1_one_dim_array/cu1402.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[10001];
+    int k;
+    scanf("%d", &k);
+    for(int i = 0; i < k ; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(int i = k-1 ; i >= 0 ; i--)
+    {
+        printf("%d ", a[i]);
+    }
+}
+
+
