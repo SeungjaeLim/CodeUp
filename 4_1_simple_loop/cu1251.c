1e3c5a140e0874fcf54652652abf17d304aaeee8
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-29 23:16:27 +0900

    Solve cu1251, cu1351

---
+++ 4_1_simple_loop/cu1251.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    for(int i = 1 ; i <= 100 ; i++)
+    {
+        printf("%d\n",i);
+    }
+}
+
---
+++ 4_2_nested_loop/cu1351.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    for(int i = n;i<=k;i++)
+    {
+        for(int j = 1; j <= 9; j++)
+        {
+            printf("%d*%d=%d\n",i,j,i*j);
+        }
+    }
+    return 0;
+}
+
