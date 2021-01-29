3122ec062d93a76553c6557341c1c1f4b9d9de6e
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-01-29 23:10:06 +0900

    Solve cu1093

---
+++ 5_1_one_dim_array/cu1093.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int student[24] = {0,};
+    int k, n;
+    scanf("%d", &k);
+    for(int i = 0 ; i < k ; i++ )
+    {
+        scanf("%d", &n);
+        student[n]++;
+    }
+    for(int i = 1 ; i < 24 ; i++)
+    {
+        printf("%d ", student[i]);
+    }
+    return 0;
+}
+
