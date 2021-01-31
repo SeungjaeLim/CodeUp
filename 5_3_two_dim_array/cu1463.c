2161c51da66d7b51814b56049526880a6362a020
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-01 00:39:01 +0900

    Solve cu1463

---
+++ 5_3_two_dim_array/cu1463.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[100][100];
+    int n;
+    int cnt = 1;
+    scanf("%d",&n);
+    for(int j = 0 ; j < n ; j++)
+    {
+        for(int i = n-1 ; i >= 0 ; i--)
+        {
+            a[i][j] = cnt;
+            cnt++;
+        }
+    }
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
