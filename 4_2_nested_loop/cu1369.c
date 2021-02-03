e82e398688ab3f1f142a9340752251d55cb72d9e
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-03 22:50:45 +0900

    Solve cu1369

---
+++ 4_2_nested_loop/cu1369.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    int r = 0;
+    scanf("%d %d", &n, &k);
+    for(int i = 1;i<=n;i++)
+    {
+        for(int j = 1; j<=n;j++)
+        {
+            if(i == 1 || i == n || j == 1 || j == n)
+            {
+                printf("*");
+            }
+            else
+            {
+                if( j%k == r%k )
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+        }
+        r = r + k-1;
+        printf("\n");
+    }
+    return 0;
+}
+
