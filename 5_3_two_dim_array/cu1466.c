7488d5e319758d4b08ac4858349dac8c408e4eb5
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-10 23:25:16 +0900

    Solve 2dim array in order 5-8

---
+++ 5_3_two_dim_array/cu1466.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[100][100];
+    int m, n;
+    int cnt = 1;
+    scanf("%d %d",&n,&m);
+    for(int j = m-1 ; j >= 0  ; j--)
+    {
+        for(int i = n-1 ; i >= 0 ; i--)
+        {
+            a[i][j] = cnt;
+            cnt++;
+        }
+    }
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < m ; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
