578218b0e95e93a463d9444049f9b834f624d2dd
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-11 22:59:26 +0900

    Solve 2_dim_arr zigzag 1-4

---
+++ 5_3_two_dim_array/cu1468.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[100][100];
+    int n;
+    int cnt = 1;
+    scanf("%d",&n);
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(i%2 == 0)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                a[i][j] = cnt;
+                cnt++;
+            }
+        }
+        else
+        {
+            for(int j = n-1 ; j >= 0 ; j--)
+            {
+                a[i][j] = cnt;
+                cnt++;
+            }
+        }
+    }
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
---
+++ 5_3_two_dim_array/cu1470.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[100][100];
+    int n;
+    int cnt = 1;
+    scanf("%d",&n);
+    for(int j = 0 ; j < n ; j++)
+    {
+        if(j%2 == 0)
+        {
+            for(int i = 0 ; i < n ; i++)
+            {
+                a[i][j] = cnt;
+                cnt++;
+            }
+        }
+        else
+        {
+            for(int i = n-1 ; i >= 0 ; i--)
+            {
+                a[i][j] = cnt;
+                cnt++;
+            }
+        }
+    }
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < n ; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
