dbc7c8f5d4fdd968d621556ec993ae1506ad8211
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-23 23:01:30 +0900

    Solve cu1071

---
+++ 0_basic_100_problems/cu1071.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main()
+{
+    int n;
+    for( ;  ; )
+    {
+        scanf("%d",&n);
+        if( n == 0)
+        {
+            break;
+        }
+        printf("%d\n",n);
+    }
+
+}
+
